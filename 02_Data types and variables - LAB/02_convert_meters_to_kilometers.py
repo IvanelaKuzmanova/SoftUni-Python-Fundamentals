3ed6d3792112d87a9ef982d5d7e8678fe975dcbf
Ivanela Kuzmanova <155246162+IvanelaKuzmanova@users.noreply.github.com>
2024-01-02 15:54:13 +0200

    New subrepo_Data types and variables_LAB projects

---
+++ 02_Data types and variables - LAB/02_convert_meters_to_kilometers.py
@@ -0,0 +1,6 @@
+meters = int(input())
+convertor = 1000
+
+kilometers = meters / 1000
+
+print(f"{kilometers :.2f}")
---
+++ 02_Data types and variables - LAB/03_pounds_to_dollars.py
@@ -0,0 +1,6 @@
+british_pounds = int(input())
+convertor = 1.31
+
+dollars = british_pounds * convertor
+
+print(f"{dollars :.3f}")
