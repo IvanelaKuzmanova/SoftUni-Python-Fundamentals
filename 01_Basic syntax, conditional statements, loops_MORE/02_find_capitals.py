96ec9a3c053e153bd305e1075e1b2024164db93e
Ivanela Kuzmanova <155246162+IvanelaKuzmanova@users.noreply.github.com>
2024-01-08 14:41:03 +0200

    ready to push

--- 01_Basic syntax, conditional statements, loops_MORE/02_find_capitals.py
+++ 01_Basic syntax, conditional statements, loops_MORE/02_find_capitals.py
@@ -1,17 +1,21 @@
+# word = input()
+#
+# word_letters = list(word)
+# index_list = []
+#
+# for i in word_letters:
+#
+#     if str(i).isupper():
+#
+#         print(word_letters.index(i), end=" ")
+
+
 word = input()
+index_list = []
 
-word_letters = list(word)
+for i in range(len(word)):
 
+    if word[i].isupper():
+        index_list.append(i)
 
-for i in word_letters:
-
-    if str(i).isupper():
-
-        print(word_letters.index(i), end=" ")
-
-
-
-#
-# for i in range(len(word)):
-#
-#
+print(index_list)
