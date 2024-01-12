8f053836af639f8d60b4dca5648b9d9c7d797fcc
Ivanela Kuzmanova <155246162+IvanelaKuzmanova@users.noreply.github.com>
2024-01-12 12:52:40 +0200

    first push

--- 03_Lists Basics - EXERCISE/04_number_beggars.py
+++ 03_Lists Basics - EXERCISE/04_number_beggars.py
@@ -10,7 +10,8 @@ start_index_for_beggar = 0
 for n in numbers_list:         # използваме този цикъл само за да обърнем парите от стрингове в числа и да ги добавим в листа, с който ще работим
     money.append(int(n))
 
-while start_index_for_beggar < beggars:       #първият започва да взима от индекс нула, последният от индекс броя на просяците-1
+while start_index_for_beggar < beggars:      #първият започва да взима от индекс нула, последният от индекс броя на просяците-1
+#for beggar in range(beggars): - the same as the while cycle
     current_beggar_sum = 0
     for current_index in range(start_index_for_beggar, len(money), beggars):        #на всяка итерация се добавя +1 към стартовия индекс за всеки
         current_beggar_sum += money[current_index]          #ще се извърти цикъл, който ще сметне общата сума за първия
