923e42457839258afd61bfd10f2655d09ae663d3
Ivanela Kuzmanova <155246162+IvanelaKuzmanova@users.noreply.github.com>
2024-01-14 21:45:36 +0200

    first save

--- 03_Lists Basics - EXERCISE/08_seize_the_fire.py
+++ 03_Lists Basics - EXERCISE/08_seize_the_fire.py
@@ -1,23 +1,53 @@
 commands_as_list = input().split("#")
 water_amount = int(input())
 
-fire_types_list = []
-value_of_cells_list = []
+cells_put_out = []
+
+total_effort = 0
+total_fire = 0
+
+# fire_in_condition = False
 
 for element in commands_as_list:
-    command_separated = element.split("=")
+    command_separated = element.split(" = ")
     fire_type = command_separated[0]
-    water_needed = command_separated[1]
+    water_needed = int(command_separated[1])
 
-    # for i in range(len(element)):
-    #     fire_types_list.append(i[0])
-    #     value_of_cells_list.append(i[2])
-#
-#
-#
-#
+    if fire_type == "High":
+        if 81 <= water_needed <= 125:
+            if water_amount < water_needed:
+                continue
+            water_amount -= int(water_needed)
+            total_effort += 0.25 * water_needed
+            cells_put_out.append(water_needed)
+            total_fire += water_needed
 
-#
-#
-#
-# print(commands_as_list)
+    elif fire_type == "Medium":
+        if 51 <= water_needed <= 80:
+            if water_amount < water_needed:
+                continue
+            water_amount -= int(water_needed)
+            total_effort += 0.25 * water_needed
+            cells_put_out.append(water_needed)
+            total_fire += water_needed
+
+    elif fire_type == "Low":
+        if 1 <= water_needed <= 50:
+            if water_amount < water_needed:
+                continue
+            water_amount -= int(water_needed)
+            total_effort += 0.25 * water_needed
+            cells_put_out.append(water_needed)
+            total_fire += water_needed
+
+print(f"Cells:")
+
+for cell in range(len(cells_put_out)):
+    print(f" - {cells_put_out[cell]}")
+
+print(f"Effort: {total_effort :.2f}")
+print(f"Total Fire: {total_fire}")
+
+
+
+
