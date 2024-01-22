9dbb946dc6a560724b79287d4889f957f16818f5
Ivanela Kuzmanova <155246162+IvanelaKuzmanova@users.noreply.github.com>
2024-01-22 20:21:39 +0200

    first push

---
+++ 04_Functions - EXERCISE/04_odd_and_even_sum.py
@@ -0,0 +1,18 @@
+def even_odd_check(number):
+
+    even_list = []
+    odd_list = []
+
+    for digit in number:
+        if int(digit) % 2 == 0:
+            even_list.append(int(digit))
+        else:
+            odd_list.append(int(digit))
+
+    return f"Odd sum = {sum(odd_list)}, Even sum = {sum(even_list)}"
+
+number = input()
+
+result = even_odd_check(number)
+
+print(result)
