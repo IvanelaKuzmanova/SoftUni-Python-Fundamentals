f365fa2a1fd8ec9d49442ec44d35235ea3e13905
Ivanela Kuzmanova <155246162+IvanelaKuzmanova@users.noreply.github.com>
2024-01-23 12:44:09 +0200

    WIP

--- 04_Functions - EXERCISE/test.py
+++ 04_Functions - EXERCISE/test.py
@@ -2,20 +2,21 @@ current_object = input()
 valid = True
 digits_counter = 0
 
-for symbol in range(len(current_object)):
+if not 6 <= len(current_object) <= 10:
+    valid = False
+    print(f"Password must be between 6 and 10 characters")
 
-    if not 6 <= len(current_object) <= 10
+for symbol in range(len(current_object)):
     current_symbol = current_object[symbol]
 
     if current_symbol.isdigit():
         digits_counter += 1
         continue
-    elif current_object.isalpha():
-        continue
-    else:
+    elif not current_object.isupper() and not current_object.isalpha():
         valid = False
         print(f"Password must consist only of letters and digits")
         break
+
 
 if digits_counter < 2:
     print(f"Password must have at least 2 digits")
