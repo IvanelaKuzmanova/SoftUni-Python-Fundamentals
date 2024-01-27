2052b136d49779703dfeb6c0d52d2946d0274b92
Ivanela Kuzmanova <155246162+IvanelaKuzmanova@users.noreply.github.com>
2024-01-27 15:27:37 +0200

    optimized code

--- 04_Functions - EXERCISE/02_add_and_substract.py
+++ 04_Functions - EXERCISE/02_add_and_substract.py
@@ -1,15 +1,19 @@
-def sum_numbers(num1, num2):
-    result = num1 + num2
-    return result
+def sum_numbers(num1, num2) -> int:
+    return num1 + num2
 
-def substract(new_num, num3):
+
+def substract(new_num, num3) -> int:
     return new_num - num3
 
-num1 = int(input())
-num2 = int(input())
-num3 = int(input())
 
-summed = sum_numbers(num1, num2)
-difference = substract(summed, num3)
+def add_and_substract(num1, num2, num3) -> int:            #added function which calls the other two (as required in the task)
+    sum_of_first_two = sum_numbers(num1, num2)
+    final_result = substract(sum_of_first_two, num3)
+    return final_result
 
-print(difference)
+
+number_1 = int(input())
+number_2 = int(input())
+number_3 = int(input())
+
+print(add_and_substract(number_1, number_2, number_3))
