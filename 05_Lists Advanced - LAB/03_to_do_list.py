516e038e49ee0ede66ed811c5516cf36b674bbb3
Ivanela Kuzmanova <155246162+IvanelaKuzmanova@users.noreply.github.com>
2024-01-30 13:17:57 +0200

    100% working

--- 05_Lists Advanced - LAB/03_to_do_list.py
+++ 05_Lists Advanced - LAB/03_to_do_list.py
@@ -9,7 +9,7 @@ while command != "End":
 
 sorted_list = sorted(commands_list, key=lambda current_command: int(current_command.split("-")[0]))     #splitting each command to number and task, splitting the dashes and sorting all elements in the list by the element on the zero index in current command
 
-final_list = [command.split("-")[1] for command in sorted_list]     #adds all elements on index 1, ignoring dashes, to a new list
+final_list = [command.split("-")[1] for command in sorted_list]     #adds all elements on index 1 in each splitted command (ignoring dashes) to a new list
 
 print(final_list)
 
