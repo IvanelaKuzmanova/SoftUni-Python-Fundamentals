98a9714e9b81ea0374378c1ee1d7dc5fc8c45b3f
Ivanela Kuzmanova <155246162+IvanelaKuzmanova@users.noreply.github.com>
2024-01-30 21:42:37 +0200

    WIP

--- 05_Lists Advanced - LAB/07_the_office.py
+++ 05_Lists Advanced - LAB/07_the_office.py
@@ -1,7 +1,23 @@
+
+def average_happiness(happiness):
+
+    average_happiness = max(happiness) / 2
+    happy_count = 0
+
+    for person in happiness:
+        if person >= average_happiness:
+            happy_count += 1
+
+    if happy_count >= len(happiness)/2:
+        return f"Score: {happy_count}/{len(happiness)}. Employees are happy!"
+    else:
+        return f"Score: {happy_count}/{len(happiness)}. Employees are not happy!"
+
+
 happiness_list = [int(num) for num in input().split()]
 improvement_factor = int(input())
 
 total_happiness = [num * improvement_factor for num in happiness_list]
 
-print(total_happiness)
-print(happiness_list)
+check = average_happiness(total_happiness)
+print(check)
