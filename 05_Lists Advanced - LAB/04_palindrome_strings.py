d3f6e428263948713051545a2ab0cd73db795eb6
Ivanela Kuzmanova <155246162+IvanelaKuzmanova@users.noreply.github.com>
2024-01-30 16:43:13 +0200

    using function

--- 05_Lists Advanced - LAB/04_palindrome_strings.py
+++ 05_Lists Advanced - LAB/04_palindrome_strings.py
@@ -1,17 +1,21 @@
+def palindromes_check(my_list:list):
+
+    palindromes_list = []
+
+    for word in words_list:
+
+        reversed_word = word[::-1]
+
+        if word == reversed_word:
+            palindromes_list.append(word)
+
+    palindrome_count = palindromes_list.count(command)
+
+    return f"{palindromes_list}\nFound palindrome {palindrome_count} times"
+
 words_list = input().split()
 command = input()
 
-palindromes_list = []
-
-for word in words_list:
-
-    reversed_word = word[::-1]
-
-    if word == reversed_word:
-        palindromes_list.append(word)
-
-palindrome_count = palindromes_list.count(command)
-
-print(palindromes_list)
-print(f"Found palindrome {palindrome_count} times")
+result = palindromes_check(words_list)
+print(result)
 
