68b12e361a98fd6822e75b11a5f4583c4f14448c
Ivanela Kuzmanova <155246162+IvanelaKuzmanova@users.noreply.github.com>
2024-02-01 11:50:56 +0200

    WIP (80 from 100 in judge)

---
+++ 05_Lists Advanced - EXERCISE/01_which_are_in.py
@@ -0,0 +1,18 @@
+def checking_string_parts(list_1:list, list_2:list) -> list:
+
+    new_list = []
+
+    for element in list_one:        #to take each object from list one
+        for word in list_two:       #to iterate each object fro list two with each element from list one
+            if element in word:
+                [new_list.append(element) for element in list_one if element not in new_list]       #to append elements found in the list only once (and skip if already added)
+
+    return new_list
+
+
+list_one = input().split(', ')
+list_two = input().split(', ')
+
+checking = checking_string_parts(list_one, list_two)
+print(checking)
+
