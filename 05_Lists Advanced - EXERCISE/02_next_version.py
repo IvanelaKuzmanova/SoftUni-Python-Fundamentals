4941468ac930634b0418bdde1657fbd0a4f31cc2
Ivanela Kuzmanova <155246162+IvanelaKuzmanova@users.noreply.github.com>
2024-02-08 16:09:20 +0200

    100% working

--- 05_Lists Advanced - EXERCISE/02_next_version.py
+++ 05_Lists Advanced - EXERCISE/02_next_version.py
@@ -22,3 +22,16 @@
 #             break
 #
 # print(current_version)
+
+current_version = input().split('.')
+
+old_version_string = ""     #defining epmty variable for current version as whole number
+new_version_string = 0      #defining empty variable for new version as whole number
+
+
+for digit in current_version:
+    old_version_string += digit     #creating old version as string (whole number) to ensure 9 is the largest digit
+
+new_version = str(int(old_version_string) + 1)      #adding one version
+
+print(f"{new_version[0]}.{new_version[1]}.{new_version[2]}")        #printing the new one separated with dots
