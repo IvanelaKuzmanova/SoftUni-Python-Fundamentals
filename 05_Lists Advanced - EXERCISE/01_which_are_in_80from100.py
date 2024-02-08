c47b53fec7fd9497a77130e789f7f68ed677e338
Ivanela Kuzmanova <155246162+IvanelaKuzmanova@users.noreply.github.com>
2024-02-08 15:52:36 +0200

    100% working

--- 05_Lists Advanced - EXERCISE/01_which_are_in_80from100.py
+++ 05_Lists Advanced - EXERCISE/01_which_are_in_80from100.py
@@ -5,7 +5,8 @@ def checking_string_parts(list_1:list, list_2:list) -> list:
     for element in list_one:        #to take each object from list one
         for word in list_two:       #to iterate each object fro list two with each element from list one
             if element in word:
-                [new_list.append(element) for element in list_one if element not in new_list]       #to append elements found in the list only once (and skip if already added)
+                new_list.append(element)      #to append elements found in the list only once (and skip if already added)
+                break
 
     return new_list
 
