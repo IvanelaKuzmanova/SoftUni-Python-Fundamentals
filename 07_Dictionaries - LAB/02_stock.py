66b75376ea0bc5f132ca4cb6f3de6d28f7ee07a2
Ivanela Kuzmanova <155246162+IvanelaKuzmanova@users.noreply.github.com>
2024-03-06 11:37:52 +0200

    100% working

---
+++ 07_Dictionaries - LAB/02_stock.py
@@ -0,0 +1,16 @@
+content = input().split(" ")
+products_dict = {}
+
+for i in range(0, len(content), 2):     #starting from index 0 and using step 2
+    key = content[i]                    #creatng keys
+    value = content[i+1]                #creating values
+    products_dict[key] = int(value)           #assigning value to the key
+
+products_to_search = input().split()
+
+for product in products_to_search:
+    if product in products_dict:
+        print(f"We have {products_dict[product]} of {product} left")
+    else:
+        print(f"Sorry, we don't have {product}")
+
