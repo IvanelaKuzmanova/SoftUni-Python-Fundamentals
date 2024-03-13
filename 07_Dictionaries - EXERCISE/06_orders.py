e6162cfb31c2172ecff7d3a9b1663cda5a34a472
Ivanela Kuzmanova <155246162+IvanelaKuzmanova@users.noreply.github.com>
2024-03-13 08:15:46 +0200

    100% working

--- 07_Dictionaries - EXERCISE/06_orders.py
+++ 07_Dictionaries - EXERCISE/06_orders.py
@@ -10,10 +10,10 @@ while True:
     name = command[0]
     current_price = float(command[1])
     current_quantity = float(command[2])
-    current_info = {"price": current_price, "quantity": current_quantity}
+    starting_info = {"price": 0, "quantity": 0}
 
     if name not in products.keys():
-        products[name] = current_info
+        products[name] = starting_info
 
     products[name]["price"] = current_price
     products[name]["quantity"] += current_quantity
