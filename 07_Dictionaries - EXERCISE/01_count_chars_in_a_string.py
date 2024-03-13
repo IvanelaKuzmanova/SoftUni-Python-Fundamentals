be8c6468053a0eaa0dd80e7997d6ee19defbb03a
Ivanela Kuzmanova <155246162+IvanelaKuzmanova@users.noreply.github.com>
2024-03-13 16:00:28 +0200

    Added solution

--- 07_Dictionaries - EXERCISE/01_count_chars_in_a_string.py
+++ 07_Dictionaries - EXERCISE/01_count_chars_in_a_string.py
@@ -10,3 +10,21 @@ for letter in letters_list:
 
 for key, value in dictionary.items():
     print(f"{key} -> {value}")
+
+
+#advanced method:
+#
+# from collections import Counter
+#
+# def count_letter(text):
+#     text = "".join(text)
+#     chars_dict = Counter(text)
+#
+#     return chars_dict
+#
+#
+# text = input().split()
+# characters_dictionary = count_letter(text)
+#
+# for k, v in characters_dictionary.items():
+#     print(f"{k} -> {v}")
