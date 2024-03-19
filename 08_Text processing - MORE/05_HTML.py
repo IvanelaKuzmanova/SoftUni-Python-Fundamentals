72621ec960f6d9b3bfb9861b33f23e17c409c29b
Ivanela Kuzmanova <155246162+IvanelaKuzmanova@users.noreply.github.com>
2024-03-19 18:25:24 +0200

    100% working

---
+++ 08_Text processing - MORE/05_HTML.py
@@ -0,0 +1,14 @@
+title = input()
+content = input()
+
+print(f"<h1>\n\t{title}\n</h1>")
+print(f"<article>\n\t{content}\n</article>")
+
+while True:
+
+    comment = input()
+
+    if comment == "end of comments":
+        break
+
+    print(f"<div>\n\t{comment}\n</div>")
