a890ee8e2d56573e89cc1c4fab79fe8fab9b89d7
Ivanela Kuzmanova <155246162+IvanelaKuzmanova@users.noreply.github.com>
2024-03-19 18:15:43 +0200

    100% working

---
+++ 08_Text processing - MORE/04_morse_code_translator.py
@@ -0,0 +1,36 @@
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+morse_words_list = input().split("|")
+stripped_words = [word.strip() for word in morse_words_list]
+
+encrypted_list = []
+
+for word in stripped_words:
+
+    current_word_letters = word.split()
+    current_word = ""
+    for element in current_word_letters:
+        for key, value in MORSE_CODE_DICT.items():
+            if element == value:
+                current_word += key
+
+    encrypted_list.append(current_word)
+
+print(" ".join(encrypted_list))
+
+
+
