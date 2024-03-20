119cf29537a93a2350250b67fa6fbf8f54769366
Ivanela Kuzmanova <155246162+IvanelaKuzmanova@users.noreply.github.com>
2024-03-20 11:30:19 +0200

    WIP

--- 07_Dictionaries - MORE/04_snow_white.py
+++ 07_Dictionaries - MORE/04_snow_white.py
@@ -20,5 +20,24 @@ while True:
             dwarfs_register[hat_color][name] = physics
 
 #--------------------------------------------------------------------------
+colors_list = []
+dwarfs_list = []
+physics_list = []
 
 
+for key, value in dwarfs_register.items():
+
+    colors_list.append(key)
+
+    for k, v in value.items():
+
+        dwarfs_list.append(k)
+        physics_list.append(v)
+
+# for index in range(1, len(dwarfs_list + 1):
+#
+#     current_tuple =
+
+tupled_list = list(zip(colors_list, dwarfs_list, physics_list))
+
+print(sorted(tupled_list, key=lambda x: (x[2], x[1]), reverse=True))
